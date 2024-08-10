39068fef36c36faa49e2999bd8dbda6e64adb0e8
Waaazzzuuup <Waaazzzuuup@users.noreply.github.com>
2024-08-11 00:07:54 +0300

    set up card button clicking (only load level for now)

--- Source/Painter/UI/PaintPicker/PaintingGridCard.cpp
+++ Source/Painter/UI/PaintPicker/PaintingGridCard.cpp
@@ -1,7 +1,19 @@
 #include "Painter/UI/PaintPicker/PaintingGridCard.h"
 
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/StereoLayerFunctionLibrary.h"
 
-void UPaintingGridCard::SetPaintingName(FString PaintingName)
+
+void UPaintingGridCard::SetPaintingName(FString NewPaintingName)
 {
+	PaintingName = NewPaintingName;
 	SlotName->SetText(FText::FromString(PaintingName));
+	CardButton->OnClicked.AddDynamic(this, &UPaintingGridCard::CardButtonClicked);
+}
+
+void UPaintingGridCard::CardButtonClicked()
+{
+	// fix for long vr level loading 
+	UStereoLayerFunctionLibrary::ShowSplashScreen();
+	UGameplayStatics::OpenLevel(GetWorld(), "Canvas", true, "SlotName="+PaintingName);
 }
--- Source/Painter/UI/PaintPicker/PaintingGridCard.h
+++ Source/Painter/UI/PaintPicker/PaintingGridCard.h
@@ -2,6 +2,7 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Components/Button.h"
 #include "Components/TextBlock.h"
 #include "PaintingGridCard.generated.h"
 
@@ -12,9 +13,18 @@ class PAINTER_API UPaintingGridCard : public UUserWidget
 	GENERATED_BODY()
 
 public:
-	void SetPaintingName(FString PaintingName);
+	void SetPaintingName(FString NewPaintingName);
 
 protected:
 	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, meta = (BindWidget))
 	UTextBlock* SlotName;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, meta = (BindWidget))
+	UButton* CardButton;
+
+private:
+	UFUNCTION()
+	void CardButtonClicked();
+
+	FString PaintingName;
 };
