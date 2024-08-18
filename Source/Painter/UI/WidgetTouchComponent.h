7db1369cdbffe5a655e786ca5bfdbcf374728944
Waaazzzuuup <Waaazzzuuup@users.noreply.github.com>
2024-08-18 13:29:42 +0300

    created custom WidgetInteractionComp (touch)

---
+++ Source/Painter/UI/WidgetTouchComponent.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/WidgetInteractionComponent.h"
+#include "WidgetTouchComponent.generated.h"
+
+// CUSTOM: widget component override for touch-button purposes. (text here for annotation in BP)
+// this magic for component spawn in BP (copied from usual widget interaction component) 
+UCLASS(ClassGroup="UserInterface", meta=(BlueprintSpawnableComponent) )
+class PAINTER_API UWidgetTouchComponent : public UWidgetInteractionComponent
+{
+	GENERATED_BODY()
+
+public:
+	void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+};
