45c29fd20202bb94a553ba341f54741bb6d999e4
Waaazzzuuup <Waaazzzuuup@users.noreply.github.com>
2024-08-27 00:04:56 +0300

    Update PainterGameMode.cpp

--- Source/Painter/PainterGameMode.cpp
+++ Source/Painter/PainterGameMode.cpp
@@ -71,6 +71,6 @@ void APainterGameMode::DeleteLastStroke()
 void APainterGameMode::ChangeThickness()
 {
 	ThicknessIndex++;
-	if (ThicknessIndex > sizeof(Thicks)/sizeof(Thicks[0]) ) ThicknessIndex = 0;
+	if (ThicknessIndex > sizeof(Thicks)/sizeof(Thicks[0]) - 1 ) ThicknessIndex = 0;
 	UE_LOG(LogTemp, Warning, TEXT("CurrentThickness is %f"), Thicks[ThicknessIndex]);
 }
